373be9460c5410fd3dbe1e653aadf18186ea5d76
nikita <nikita.ratkevich91@mail.ru>
2022-12-25 08:26:57 +0200

    ddf

--- mail.php
+++ mail.php
@@ -1,40 +1,39 @@
-<?php 
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
+<?php
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+/**
+ * PHPMailer simple contact form example.
+ * If you want to accept and send uploads in your form, look at the send_file_upload example.
+ */
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+//Import the PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\PHPException;
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
-$mail->AltBody = '';
+if(isset($_POST['send'])) {
+    $mail = new PHPMailer(true);
+    $mail -> isSMTP();
+    $mail -> Host = 'smtp.mail.ru';
+    $mail -> SMTPAuth = true;
+    $mail -> Username = 'e6wuk1990@gmail.com';
+    $mail -> Password = 'Nekit1990!';
+    $mail -> SMTPSecture = 'ssl!';
+    $mail -> Port = '465';
 
-if(!$mail->send()) {
-    echo 'Error';
-} else {
-    header('location: thank-you.html');
-}
+    $mail -> setFrom('e6wuk1990@mail.ru');
+    $mail -> addAddress($_POST['email']);
+    $mail -> isHtml(true);
+    $mail -> Subject = $_POST['subject'];
+    $mail -> Body = $_POST['message'];
+    $mail -> send();
+
+    echo "<script>alert('sended')</script>";
+
+};
+
 ?>
